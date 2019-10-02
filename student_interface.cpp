6b3a44840a8eeaf69416be805bab356529b94cdd
mmmb16 <marta.boavida76@gmail.com>
2019-10-02 18:16:18 +0200

    git commit -m "1st assignment"

---
+++ student_interface.cpp
@@ -0,0 +1,93 @@
+#include "student_image_elab_interface.hpp"
+#include "student_planning_interface.hpp"
+
+#include <stdexcept>
+#include <sstream>
+
+#include <experimental/filesystem>
+
+namespace student {
+
+ void loadImage(cv::Mat& img_out, const std::string& config_folder){  
+   throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );
+ }
+
+static int i;
+static bool state = false;
+
+ void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
+  	
+	if (!state) {
+		i = 0;
+		state = true;
+	}
+
+	//CREATES ONE FOLDER IF DOESN'T EXISTS
+	namespace fs = std::experimental::filesystem;
+	std::stringstream src;
+	src << config_folder << "saved_images/";
+
+
+	if (!fs::is_directory(src.str()) || !fs::exists(src.str())) { 
+	    fs::create_directory(src.str()); 
+	}
+
+	//SAVES IMAGE WHEN PRESS S ON THE KEYBOARD
+
+	cv::imshow( topic, img_in);
+	char k;
+	k = cv::waitKey(30);
+
+    	std::stringstream image;
+    	
+    	switch (k) {    	
+		case 's':
+				
+			image << src.str() << std::setfill('0') << std::setw(4) << (i++) << ".jpg";
+			std::cout << image.str() << std::endl;
+			cv::imwrite( image.str(), img_in );
+
+			std::cout << "The image" << image.str() << "was saved." << std::endl;
+		 	break;
+		default:
+			//std::cout << "TRENTO " << k << std::endl;
+				break;
+	} 
+
+  }
+
+  bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );   
+  }
+
+  void imageUndistort(const cv::Mat& img_in, cv::Mat& img_out, 
+          const cv::Mat& cam_matrix, const cv::Mat& dist_coeffs, const std::string& config_folder){
+
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );  
+
+  }
+
+  void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec, const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, cv::Mat& plane_transf, const std::string& config_folder){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );  
+  }
+
+
+  void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, const double scale, const std::string& config_folder){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );   
+  }
+
+  bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );   
+  }
+
+  bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );    
+  }
+
+  bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path){
+    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );     
+  }
+
+
+}
+
