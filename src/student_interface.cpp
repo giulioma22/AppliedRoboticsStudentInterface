8a5f0016af4af98c95e6a2dd6e029402cc8a38c4
giulioma22 <giuliomartinelli22@gmail.com>
2019-10-10 18:40:14 +0200

    Add detect functions

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -87,23 +87,192 @@ static bool state = false;
   }
  
   void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec, const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, const std::vector<cv::Point2f>& dest_image_points_plane, cv::Mat& plane_transf, const std::string& config_folder){
-
     cv::Mat image_points;
+    // projectPoint output is image_points
     cv::projectPoints(object_points_plane, rvec, tvec, cam_matrix, cv::Mat(), image_points);
     plane_transf = cv::getPerspectiveTransform(image_points, dest_image_points_plane);
-
   }
 
   void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, const std::string& config_folder){
     cv::warpPerspective(img_in, img_out, transf, img_in.size());
   }
 
+
+  bool detect_red(const cv::Mat& hsv_img, const double scale, std::vector<Polygon>& obstacle_list){
+    
+    cv::Mat red_mask_low, red_mask_high, red_mask;
+    cv::inRange(hsv_img, cv::Scalar(0, 72, 105), cv::Scalar(20, 255, 255), red_mask_low);
+    cv::inRange(hsv_img, cv::Scalar(175, 10, 10), cv::Scalar(179, 255, 255), red_mask_high);
+    cv::addWeighted(red_mask_low, 1.0, red_mask_high, 1.0, 0.0, red_mask);
+    
+    // Find red regions
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+    // Process red mask
+    cv::findContours(red_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i=0; i<contours.size(); ++i)
+    {
+      // Approximate polygon w/ fewer vertices if not precise
+      // 3rd arg - max distance original curve to approx
+      approxPolyDP(contours[i], approx_curve, 3, true);
+
+      Polygon scaled_contour;
+      for (const auto& pt: approx_curve) {
+        scaled_contour.emplace_back(pt.x/scale, pt.y/scale);
+      }
+      // Add obstacle to list
+      obstacle_list.push_back(scaled_contour);
+    }
+
+    return true;
+  }
+
+  bool detect_green_victims(const cv::Mat& hsv_img, const double scale, std::vector<std::pair<int,Polygon>>& victim_list){
+    
+    cv::Mat green_mask_victims;
+    cv::inRange(hsv_img, cv::Scalar(50, 80, 34), cv::Scalar(75, 255, 255), green_mask_victims);
+
+    // Find green regions - VICTIMS
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+    // Process green mask - VICTIMS
+    cv::findContours(green_mask_victims, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    for (int i=0; i<contours.size(); ++i)
+    {
+      // Approximate polygon w/ fewer vertices if not precise
+      approxPolyDP(contours[i], approx_curve, 1, true);
+
+      Polygon scaled_contour;
+      for (const auto& pt: approx_curve) {
+        scaled_contour.emplace_back(pt.x/scale, pt.y/scale);
+      }
+      // Add victim to list
+      victim_list.push_back({i+1, scaled_contour});
+    }
+    
+    return true;
+  }
+
+  bool detect_green_gate(const cv::Mat& hsv_img, const double scale, Polygon& gate){
+
+    cv::Mat green_mask_gate;    
+    cv::inRange(hsv_img, cv::Scalar(50, 80, 34), cv::Scalar(75, 255, 255), green_mask_gate);
+    
+    // Find green regions - GATE
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+    // Process green mask - GATE
+    cv::findContours(green_mask_gate, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    
+    bool gate_found = false;
+
+    for(auto& contour : contours){
+      const double area = cv::contourArea(contour);
+      if (area > 500){
+        // Approximate polygon w/ fewer vertices if not precise
+        approxPolyDP(contour, approx_curve, 3, true);
+
+        for (const auto& pt: approx_curve) {
+          // Store (scaled) values of gate
+          gate.emplace_back(pt.x/scale, pt.y/scale);
+        }
+        gate_found = true;
+        break;
+      }      
+    }
+    
+    return gate_found;
+  }
+
+  bool detect_blue_robot(const cv::Mat& hsv_img, const double scale, Polygon& triangle, double& x, double& y, double& theta){
+
+    cv::Mat blue_mask;    
+    cv::inRange(hsv_img, cv::Scalar(92, 80, 50), cv::Scalar(145, 255, 255), blue_mask);
+
+    // Process blue mask
+    std::vector<std::vector<cv::Point>> contours, contours_approx;
+    std::vector<cv::Point> approx_curve;
+    // Find the contours of blue objects
+    cv::findContours(blue_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    bool robot_found = false;
+    for (int i=0; i<contours.size(); ++i)
+    {
+      // Approximate polygon w/ fewer vertices if not precise
+      cv::approxPolyDP(contours[i], approx_curve, 30, true);
+      if (approx_curve.size() != 3) continue;
+      robot_found = true;
+      break;
+    }
+
+    if (robot_found) 
+    {
+      // Store values in triangle
+      for (const auto& pt: approx_curve) {
+        triangle.emplace_back(pt.x/scale, pt.y/scale);
+      }
+
+      // Find center of robot
+      double cx, cy;
+      for (auto item: triangle) 
+      {
+        cx += item.x;
+        cy += item.y;
+      }
+      cx /= triangle.size();
+      cy /= triangle.size();
+
+      // Find further vertix from center
+      double dst = 0;
+      Point vertex;
+      for (auto& item: triangle)
+      {
+        double dx = item.x-cx;      
+        double dy = item.y-cy;
+        double curr_d = dx*dx + dy*dy;
+        if (curr_d > dst)
+        { 
+          dst = curr_d;
+          vertex = item;
+        }
+      }
+
+      // Calculate yaw
+      double dx = cx-vertex.x;
+      double dy = cy-vertex.y;
+
+      // Robot position
+      x = cx;
+      y = cy;
+      theta = std::atan2(dy, dx);
+    }
+
+    return robot_found;
+
+}
+
   bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED 3" );   
+    cv::Mat hsv_img;
+    cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
+    
+    // Check if objects found
+    const bool red = detect_red(hsv_img, scale, obstacle_list);
+    if(!red) std::cout << "detect_red returns false" << std::endl;
+    const bool green_gate = detect_green_gate(hsv_img, scale, gate);
+    if(!green_gate) std::cout << "detect_green_gate returns false" << std::endl;
+    const bool green_victims = detect_green_victims(hsv_img, scale, victim_list);
+    if(!green_victims) std::cout << "detect_green_victims returns false" << std::endl;
+
+    return red && green_gate && green_victims;
   }
 
   bool findRobot(const cv::Mat& img_in, const double scale, Polygon& triangle, double& x, double& y, double& theta, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED 4" );    
+    cv::Mat hsv_img;
+    cv::cvtColor(img_in, hsv_img, cv::COLOR_BGR2HSV);
+    // RGB to HSV to then detect blue of robot more easily
+    return detect_blue_robot(hsv_img, scale, triangle, x, y, theta);    
   }
 
   bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path){
