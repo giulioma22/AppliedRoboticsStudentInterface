c3a985942abde537cd0b1e5b0eabb1204cdcccdb
giulioma22 <giuliomartinelli22@gmail.com>
2019-11-22 17:11:44 +0100

    Add Dubins script

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -397,16 +397,8 @@ static bool state = false;
 
   bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list, const std::vector<std::pair<int,Polygon>>& victim_list, const Polygon& gate, const float x, const float y, const float theta, Path& path, const std::string& config_folder){
   
-    float xc = 0, yc = 1.5, r = 1.4;
-    float ds = 0.05;
-    for (float theta = -M_PI/2, s = 0; theta<(-M_PI/2 + 1.2); theta+=ds/r, s+=ds) {
-      path.points.emplace_back(s, xc+r*std::cos(theta), yc+r*std::sin(theta), theta+M_PI/2, 1./r);    
-    }
 
-    return true;
-
-  }
-
+  }    
 
 }
 
